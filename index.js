e8636cd3771ac4267c84cee3d13ff047448a37b8
Hasar Ali <hasar.ali@protonmail.com>
2021-05-23 22:42:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,6 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials.map(sentence =>  sentence.toLowerCase().split(' ')
-                                      .map(word => word.charAt(0).toUpperCase().join))
+                                      .map(word => word.charAt(0).toUpperCase() + word.slice(1)))
+                                      .join(' ')
 }
